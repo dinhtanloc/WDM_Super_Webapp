072660f62d29f29891e1ba97c60ec4fe0cbe0b2b
dinhtanloc <locdinh.31221020226@st.ueh.edu.vn>
2025-07-01 12:46:21 +0700

    Fix: fix structured

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv, find_dotenv
+import os
+load_dotenv(find_dotenv())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wx_orusn=o_j0kiy!2&ilob11!ort0(*ok_)qdj)lk!68!km_%'
+SECRET_KEY = os.getenv('DJANGO_BACKEND_KEYS')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
